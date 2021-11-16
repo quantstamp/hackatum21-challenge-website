d24288a26ef69eb8e81a89377086fa59bf819c5e
Mohsen Ahmadvand <mohsen.ahmadv@gmail.com>
2021-11-16 16:38:21 +0100

    added score and penalization of failing test cases

--- configs.js
+++ configs.js
@@ -2,8 +2,10 @@ const TOKEN_ADDRESS = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F"; //HAK TOKEN 
 const TEAMS = [
     {name:"team1", 
     wallet:"0x2a38C5fd91aefC5C3983AdF9592DE737740b27cc", 
-    bank:"0x2a38C5fd91aefC5C3983AdF9592DE737740b27cc"},
+    bank:"0x2a38C5fd91aefC5C3983AdF9592DE737740b27cc",
+    failed_tests:1},
     {name:"team2", 
     wallet:"0x2a38C5fd91aefC5C3983AdF9592DE737740b27cc", 
-    bank:"0x2a38C5fd91aefC5C3983AdF9592DE737740b27cc"},
+    bank:"0x2a38C5fd91aefC5C3983AdF9592DE737740b27cc",
+    failed_tests:0},
 ]
